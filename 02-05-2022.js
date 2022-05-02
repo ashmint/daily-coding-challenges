2fd76e17a9d8b95d18712d7b3871b149833b77e9
Ashish Tom <ashmint@gmail.com>
2022-05-02 23:33:37 +0530

    modified problem description

--- 02-05-2022.js
+++ 02-05-2022.js
@@ -22,7 +22,7 @@ var uniqueInOrder=function(iterable){
     return arr;
     }
 
-/* Fake Binary
+/*8 Kyu - Fake Binary
 
 Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
 
