d04fbf48ae5a7bae6949d2dd3094c786218e0313
Ashish Tom <ashmint@gmail.com>
2022-05-02 23:55:08 +0530

    added solution

--- 02-05-2022.js
+++ 02-05-2022.js
@@ -36,3 +36,17 @@ function fakeBin(x){
     return [...x].map(x => x < 5 ? '0' : '1' ).join('')
   }
 
+/* 8 Kyu - Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+invert([]) == []
+
+*/
+
+//SOLUTION
+
+function invert(array) { 
+  return array.map(x => x * -1)  
+}
+
