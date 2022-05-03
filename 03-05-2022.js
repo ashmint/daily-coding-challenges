36d08399aa1600bdd752b8a5ac7162ddaf0cee64
Ashish Tom <ashmint@gmail.com>
2022-05-03 22:27:33 +0530

    added solution

--- 03-05-2022.js
+++ 03-05-2022.js
@@ -1,3 +1,41 @@
+/* 5 Kyu - Valid Parentheses
+
+Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
+
+Examples
+"()"              =>  true
+")(()))"          =>  false
+"("               =>  false
+"(())((()())())"  =>  true
+Constraints
+0 <= input.length <= 100
+
+
+*/
+
+//SOLUTION
+
+function validParentheses(parens) {
+  let result = true
+  let lb = 0 
+  let i = 0;
+  if ((parens[0] === ')') || (parens.length === 1) || (parens[parens.length-1] === "(" )){
+    return false
+  }
+  else{
+    while(i <= (parens.length - 1)){
+      if(lb < 0){
+        break;
+      }
+      parens[i] === '(' ? lb++ : lb--
+      i++;
+      }
+    return lb === 0 ? true : false
+    }
+  }
+
+
+
 /* 6 Kyu - Build Tower
 
 Build a pyramid-shaped tower given a positive integer number of floors. A tower block is represented with "*" character.
