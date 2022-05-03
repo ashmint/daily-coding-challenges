0d88b16c377d88dd70b9bd5701c3d45cfadb1ec6
Ashish Tom <ashmint@gmail.com>
2022-05-04 00:12:11 +0530

    added solution

---
+++ 04-05-2022.js
@@ -0,0 +1,16 @@
+/* 8 Kyu - Double Char
+Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+Examples (Input -> Output):
+* "String"      -> "SSttrriinngg"
+* "Hello World" -> "HHeelllloo  WWoorrlldd"
+* "1234!_ "     -> "11223344!!__  "
+Good Luck!
+
+*/
+
+//SOLUTION
+
+function doubleChar(str) {
+    return str.split('').map(x => x.repeat(2)).join('')
+  }
