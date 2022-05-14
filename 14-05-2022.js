54e2ea757c0790c26005e295cba6cf586814e65c
Ashish Tom <ashmint@gmail.com>
2022-05-14 23:57:36 +0530

    added solutions

---
+++ 14-05-2022.js
@@ -0,0 +1,61 @@
+/* 7 Kyu - You're a square!
+
+A square of squares
+You like building blocks. You especially like building blocks that are squares. And what you even like more, is to arrange them into a square of square building blocks!
+
+However, sometimes, you can't arrange them into a square. Instead, you end up with an ordinary rectangle! Those blasted things! If you just had a way to know, whether you're currently working in vain… Wait! That's it! You just have to check if your number of building blocks is a perfect square.
+
+Task
+Given an integral number, determine if it's a square number:
+
+In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+
+The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+
+Examples
+-1  =>  false
+ 0  =>  true
+ 3  =>  false
+ 4  =>  true
+25  =>  true
+26  =>  false
+
+*/ 
+
+//SOLUTION 
+
+
+var isSquare = function(n){
+    return n === - 1 ? false : Math.sqrt(n) % 1 === 0 ? true : false
+  }
+
+
+
+/* 8 Kyu - Grasshopper - Check for factor
+
+This function should test if the factor is a factor of base.
+
+Return true if it is a factor or false if it is not.
+
+About factors
+Factors are numbers you can multiply together to get another number.
+
+2 and 3 are factors of 6 because: 2 * 3 = 6
+
+You can find a factor by dividing numbers. If the remainder is 0 then the number is a factor.
+You can use the mod operator (%) in most languages to check for a remainder
+For example 2 is not a factor of 7 because: 7 % 2 = 1
+
+Note: base is a non-negative number, factor is a positive number.
+
+
+*/
+
+
+//SOLUTION 
+
+
+function checkForFactor (base, factor) {
+    return base%factor === 0 ? true : false;
+  }
+
