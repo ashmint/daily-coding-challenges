5d76c336e93901d3c2706531f3fa35090ef96ddc
Ashish Tom <ashmint@gmail.com>
2022-05-15 22:28:52 +0530

    added solution

--- 15-05-2022.js
+++ 15-05-2022.js
@@ -10,3 +10,31 @@ Complete the method that takes a boolean value and return a "Yes" string for tru
 function boolToWord( bool ){
     return bool ? 'Yes':'No';
   }
+
+
+
+/* 7 Kyu - Don't give me five!
+
+
+Don't give me five!
+In this kata you get the start number and the end number of a region and should return the count of all numbers except numbers with a 5 in it. The start and the end number are both inclusive!
+
+Examples:
+
+1,9 -> 1,2,3,4,6,7,8,9 -> Result 8
+4,17 -> 4,6,7,8,9,10,11,12,13,14,16,17 -> Result 12
+The result may contain fives. ;-)
+The start number will always be smaller than the end number. Both numbers can be also negative!
+
+*/
+
+
+//SOLUTION 
+
+function dontGiveMeFive(start, end)
+{
+  let arr = [...Array((end-start) + 1)].map((_, i) => start + i );  //Populates an array with elements from start to end
+  return arr.filter(x => ! (x +'').split('').includes('5')).length  // Filters the array for elements containing a 5 in it
+  
+}
+
