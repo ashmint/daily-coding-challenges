4db4dd050ce165783b3fcd99c429c9fbf3f01548
Ashish Tom <ashmint@gmail.com>
2022-05-17 21:57:18 +0530

    added solution

--- 17-05-2022.js
+++ 17-05-2022.js
@@ -41,3 +41,24 @@ var countBits = function (n) {
             .filter( x => x === '1')
             .length
 }
+
+/* 8 Kyu - Find the smallest integer in the array
+
+
+Given an array of integers your solution should find the smallest integer.
+
+For example:
+
+Given [34, 15, 88, 2] your solution will return 2
+Given [34, -345, -1, 100] your solution will return -345
+You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+*/
+
+//SOLUTION
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      return Math.min(...args)
+    }
+  }
