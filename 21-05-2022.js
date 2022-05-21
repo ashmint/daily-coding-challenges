e946ed3638eb0a84f41e1876eeeb570de5862df2
Ashish Tom <ashmint@gmail.com>
2022-05-21 20:21:54 +0530

    added solution

---
+++ 21-05-2022.js
@@ -0,0 +1,21 @@
+/* 7 Kyu - Consecutive items
+
+You are given a list of unique integers arr, and two integers a and b. Your task is to find out whether or not a and b appear consecutively in arr, and return a boolean value (True if a and b are consecutive, False otherwise).
+
+It is guaranteed that a and b are both present in arr.
+
+
+*/
+
+//SOLUTION 
+
+function consecutive(arr, a, b) {
+    let value = false
+    for(i=0;i<arr.length-1;i++){
+      if ((arr[i] === a && arr[i+1] === b) || (arr[i] === b && arr[i+1] === a)){
+        value = true;
+      }
+    }
+    return value
+  }
+
