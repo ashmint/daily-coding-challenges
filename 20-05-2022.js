1fddbbd25ecd43644c8574cd4b38fbebe9b2aeba
Ashish Tom <ashmint@gmail.com>
2022-05-21 20:19:15 +0530

    added solutions

---
+++ 20-05-2022.js
@@ -0,0 +1,54 @@
+/* 6 Kyu - Mexican Wave
+
+The wave (known as the Mexican wave in the English-speaking world outside North America) is an example of metachronal rhythm achieved in a packed stadium when successive groups of spectators briefly stand, yell, and raise their arms. Immediately upon stretching to full height, the spectator returns to the usual seated position.
+
+The result is a wave of standing spectators that travels through the crowd, even though individual spectators never move away from their seats. In many large arenas the crowd is seated in a contiguous circuit all the way around the sport field, and so the wave is able to travel continuously around the arena; in discontiguous seating arrangements, the wave can instead reflect back and forth through the crowd. When the gap in seating is narrow, the wave can sometimes pass through it. Usually only one wave crest will be present at any given time in an arena, although simultaneous, counter-rotating waves have been produced. (Source Wikipedia)
+Task
+In this simple Kata your task is to create a function that turns a string into a Mexican Wave. You will be passed a string and you must return that string in an array where an uppercase letter is a person standing up. 
+Rules
+ 1.  The input string will always be lower case but maybe empty.
+
+ 2.  If the character in the string is whitespace then pass over it as if it was an empty seat
+Example
+wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+
+Good luck and enjoy!
+
+*/
+
+//SOLUTION 
+
+
+const wave = str => 
+  [...str].map((s, i) => 
+      str.slice(0, i) + s.toUpperCase() + str.slice(i + 1) 
+  ).filter(x => x != str);
+
+
+
+ /* 8 Kyu - Sum The Strings
+ 
+ Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+Example: (Input1, Input2 -->Output)
+
+"4",  "5" --> "9"
+"34", "5" --> "39"
+"", "" --> "0"
+"2", "" --> "2"
+"-5", "3" --> "-2"
+Notes:
+
+If either input is an empty string, consider it as zero.
+
+Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)
+
+*/
+
+
+//SOLUTION 
+
+
+function sumStr(a,b) {
+    return (+a+ +b)+''  
+  }
