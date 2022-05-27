116b0e447edb718e22bf0bf90d35a0bf6650935f
Ashish Tom <ashmint@gmail.com>
2022-05-27 23:39:02 +0530

    added solutions

---
+++ 27-05-2022.js
@@ -0,0 +1,49 @@
+/* 7 Kyu - Mumbling
+
+This time no story, no theory. The examples below show you how to write function accum:
+
+Examples:
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+*/
+
+
+//SOLUTION 
+
+
+function accum(s) {
+    arr = s.split('')
+    arr.forEach((x,i) => arr[i] = ( x.toUpperCase() + x.toLowerCase().repeat(i)) )
+    return arr.join('-')
+  }
+
+
+
+  
+/* 8 Kyu - DNA to RNA Conversion
+
+
+Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+Create a function which translates a given DNA string into RNA.
+
+For example:
+
+"GCAT"  =>  "GCAU"
+The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+
+
+*/
+
+
+//SOLUTION 
+
+
+function DNAtoRNA(dna) {
+    return dna.split('').map(x => x === 'T' ? 'U' : x).join('');
+  }
