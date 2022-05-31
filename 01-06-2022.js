b121f03f9be17d5d437618c98e64e4af7263f77f
Ashish Tom <ashmint@gmail.com>
2022-06-01 04:20:12 +0530

    added solutions

---
+++ 01-06-2022.js
@@ -0,0 +1,11 @@
+/* 8 Kyu - Remove String Spaces
+
+Simple, remove the spaces from the string, then return the resultant string.
+
+*/
+
+
+//SOLUTION 
+
+
+function noSpace(x){return x.split(' ').join('')}
