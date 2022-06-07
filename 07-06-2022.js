601fe354a51ec8733eaa423f0e4d92d0870a9a05
Ashish Tom <ashmint@gmail.com>
2022-06-07 23:48:26 +0530

    added solution

--- 07-06-2022.js
+++ 07-06-2022.js
@@ -18,4 +18,36 @@ function nameShuffler(str){
     return str.split(' ').reverse().join(" ");
   }
 
-  
+
+/* 7 Kyu - Find the vowels
+
+
+We want to know the index of the vowels in a given word, for example, there are two vowels in the word super (the second and fourth letters).
+
+So given a string "super", we should return a list of [2, 4].
+
+Some examples:
+Mmmm  => []
+Super => [2,4]
+Apple => [1,5]
+YoMama -> [1,2,4,6]
+NOTES
+Vowels in this context refers to: a e i o u y (including upper case)
+This is indexed from [1..n] (not zero indexed!)
+
+
+*/
+
+
+//SOLUTION
+
+
+function vowelIndices(word){
+    let arr = []
+    word.split('').forEach((x,i) => {
+      if (['a','e','i','o','u', 'y'].includes(x.toLowerCase()) === true) {
+      arr.push(i+1)
+      }
+    });
+    return arr
+  }
