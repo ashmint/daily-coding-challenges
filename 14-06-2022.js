a90ffa621d40105f313e87be28a83882a15534b7
Ashish Tom <ashmint@gmail.com>
2022-06-14 21:26:52 +0530

    added solution

--- 14-06-2022.js
+++ 14-06-2022.js
@@ -80,3 +80,38 @@ function createPhoneNumber(numbers) {
 }
 
 
+/* 7 Kyu - Sum of a sequence
+
+
+Your task is to make function, which returns the sum of a sequence of integers.
+
+The sequence is defined by 3 non-negative values: begin, end, step (inclusive).
+
+If begin value is greater than the end, function should returns 0
+
+Examples
+
+2,2,2 --> 2
+2,6,2 --> 12 (2 + 4 + 6)
+1,5,1 --> 15 (1 + 2 + 3 + 4 + 5)
+1,5,3  --> 5 (1 + 4)
+This is the first kata in the series:
+
+Sum of a sequence (this kata)
+Sum of a Sequence [Hard-Core Version]
+
+*/
+
+
+//SOLUTION
+
+
+const sequenceSum = (begin, end, step) => {
+    let sum = 0
+    for (i = begin; i <= end; i += step) {
+        sum += i
+    }
+    return sum
+};
+
+
