d1c921dd20212cd21744ec6090315526b5e29c29
Ashish Tom <ashmint@gmail.com>
2022-06-15 23:49:34 +0530

    added solution

---
+++ 15-06-2022.js
@@ -0,0 +1,40 @@
+/* 4 Kyu - Range Extraction
+
+A format for expressing an ordered list of integers is to use a comma separated list of either
+
+individual integers
+or a range of integers denoted by the starting integer separated from the end integer in the range by a dash, '-'. The range includes all integers in the interval including both endpoints. It is not considered a range unless it spans at least 3 numbers. For example "12,13,15-17"
+Complete the solution so that it takes a list of integers in increasing order and returns a correctly formatted string in the range format.
+
+Example:
+
+solution([-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]);
+// returns "-10--8,-6,-3-1,3-5,7-11,14,15,17-20"
+
+*/
+
+
+//SOLUTION 
+
+function solution(list){
+    let result = "";
+    let isRange = false;
+  
+    for (let i = 0; i < list.length; ++i)
+    {
+      const j = list[i];
+  
+      if (list[i + 1] !== j + 1)
+      {
+        if (isRange) result += (list[i] === list[i - 2] + 2) ? "-" : ",";
+        result += j + ",";
+        isRange = false;
+      }
+      else if (!isRange)
+      {
+        result += j;
+        isRange = true;
+      }
+    }
+    return result.slice(0, -1);
+  }
