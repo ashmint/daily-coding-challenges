41353747d29957c1d6ed9ce04e3e0baacc77eaa9
Ashish Tom <ashmint@gmail.com>
2022-06-27 20:40:55 +0530

    added solution

--- 28-06-2022.js
+++ 28-06-2022.js
@@ -20,3 +20,32 @@ Some cases:
 function multipleOfIndex(array) {
     return array.filter((x, i) => x % i === 0)
 }
+
+
+
+/* 6 Kyu - Sort the odd
+
+Task
+You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+Examples
+[7, 1]  =>  [1, 7]
+[5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+[9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+
+*/
+
+
+//SOLUTION 
+
+
+function sortArray(array) {
+    let arr = array.filter(x => x % 2 !== 0).sort((a, b) => a - b)
+    array.forEach((y, i) => {
+        if (y % 2 === 0) {
+            arr.splice(i, 0, y)
+        }
+    });
+    return arr
+}
