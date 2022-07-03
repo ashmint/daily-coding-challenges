e3beea2cfe984fab7765a8dc85059a605009093f
Ashish Tom <ashmint@gmail.com>
2022-07-03 21:52:06 +0530

    added solution

---
+++ 03-07-2022.js
@@ -0,0 +1,20 @@
+/* 8 Kyu - Find the position!
+
+When provided with a letter, return its position in the alphabet.
+
+Input :: "a"
+
+Ouput :: "Position of alphabet: 1"
+
+This kata is meant for beginners. Rank and upvote to bring it out of beta
+
+*/
+
+
+//SOLUTION 
+
+
+function position(letter) {
+    let arr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    return `Position of alphabet: ${arr.indexOf(letter) + 1}`;
+}
