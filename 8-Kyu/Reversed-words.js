ae551c75980c8a64e539485c63a2c6370afc4412
Ashish Tom <ashmint@gmail.com>
2023-01-10 10:44:32 +0530

    added 8 Kyu solution

---
+++ 8-Kyu/Reversed-words.js
@@ -0,0 +1,25 @@
+/* 
+
+8 Kyu - Reversed Words
+
+
+Complete the solution so that it reverses all of the words within the string passed in.
+
+Words are separated by exactly one space and there are no leading or trailing spaces.
+
+Example(Input --> Output):
+
+"The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+
+*/
+
+
+
+//SOLUTION 
+
+
+
+function reverseWords(str) {
+    return str.split(' ').reverse().join(' ');
+}
