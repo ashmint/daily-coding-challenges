360be1d40374d8137552e214dfdab8ba94eab724
Ashish Tom <ashmint@gmail.com>
2023-03-24 22:52:35 +0530

    added 7kyu solution

--- 28-05-2022.js
+++ 28-05-2022.js
@@ -1,22 +1,3 @@
-/* 7 Kyu - Shortest Word
-
-
-Simple, given a string of words, return the length of the shortest word(s).
-
-String will never be empty and you do not need to account for different data types.
-
-
-*/
-
-
-//SOLUTION 
-
-
-function findShort(s){
-    return s.split(' ').sort((a,b) => a.length - b.length)[0].length;
-  }
-
-
 
 
 /* 8 Kyu - What is between?
