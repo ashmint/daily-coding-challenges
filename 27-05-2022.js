6f41de32232fc8e018eeb3e8497cd1af82d8de01
Ashish Tom <ashmint@gmail.com>
2023-04-12 22:08:33 +0530

    added 7kyu solution

--- 27-05-2022.js
+++ 27-05-2022.js
@@ -1,28 +1,4 @@
-/* 7 Kyu - Mumbling
-
-This time no story, no theory. The examples below show you how to write function accum:
-
-Examples:
-accum("abcd") -> "A-Bb-Ccc-Dddd"
-accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
-accum("cwAt") -> "C-Ww-Aaa-Tttt"
-The parameter of accum is a string which includes only letters from a..z and A..Z.
-
-*/
-
-
-//SOLUTION 
-
-
-function accum(s) {
-    arr = s.split('')
-    arr.forEach((x,i) => arr[i] = ( x.toUpperCase() + x.toLowerCase().repeat(i)) )
-    return arr.join('-')
-  }
-
-
-
-  
+ 
 /* 8 Kyu - DNA to RNA Conversion
 
 
