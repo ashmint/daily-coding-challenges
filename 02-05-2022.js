7646e425f5ac2468ef7e0d36b20b9b3bad7627b3
Ashish Tom <ashmint@gmail.com>
2023-04-25 23:28:04 +0530

    added 8kyu solution

--- 02-05-2022.js
+++ 02-05-2022.js
@@ -1,16 +1,3 @@
-/*8 Kyu - Fake Binary
-
-Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
-
-Note: input will never be an empty string
-
-*/
-
-//SOLUTION
-
-function fakeBin(x){
-    return [...x].map(x => x < 5 ? '0' : '1' ).join('')
-  }
 
 /* 8 Kyu - Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
 
